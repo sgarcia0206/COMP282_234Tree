992be6c0fb8b7b19ce9d8861dc6c559ef2e9ab28
sgarcia0206 <garcia.sergio6442@gmail.com>
2024-01-18 02:26:28 -0800

    input 1

---
+++ TwoThreeFourTree.java
@@ -0,0 +1,15 @@
+/******************************************************************************
+Sergio Garcia
+COMP 282 Vasikarla S.
+09/30/2023
+
+TASK
+
+- Trace the operation of a 2-3-4 Tree given the following operations...
+    handrawn
+- Starting with an empty tree, perform the following insertion operations...
+insert: 17, 15, 49, 34, 76, 59, 97, 69, 46, 86, 20, 99, 22, 52, 89, 57, 10, 41, 75, 37.
+then... 
+delete: 99, 22, 69, 15, 10, 75
+
+*******************************************************************************/
